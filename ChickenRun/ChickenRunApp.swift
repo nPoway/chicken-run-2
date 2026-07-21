16fc045858759f9a7e1d95212021ff07ac338757
npoway <n.smoliar2013@gmail.com>
2026-07-21 12:11:03 +0300

    Initial Commit

---
+++ ChickenRun/ChickenRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenRunApp.swift
+//  ChickenRun
+//
+//  Created by Никита on 21.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChickenRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
